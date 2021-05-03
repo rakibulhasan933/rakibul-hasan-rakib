9ce98504097d72d35187fb78785cd78f3f697d3a
rakibulhasan933 <rakibulmd933@gmail.com>
2021-05-03 15:11:58 +0600

    full containted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
-import firebase from "firebase/app";
-import "firebase/auth";
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,14 +13,6 @@ import Contact from './components/Contact/Contact';
 import NotFound from './components/NotFound/NotFound';
 import Navbar from './components/Home/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-import firebaseConfig from './firebase.config';
-
-// TODO: Replace the following with your app's Firebase project configuration
-// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
--- src/firebase.config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBnBO2qHWuseWGSDXK_AW5zfmPzhB3E-lM",
-    authDomain: "rakibulhasan-rakib.firebaseapp.com",
-    projectId: "rakibulhasan-rakib",
-    storageBucket: "rakibulhasan-rakib.appspot.com",
-    messagingSenderId: "899771758563",
-    appId: "1:899771758563:web:07c3b56e4887bd257a12d6"
-};
-export default firebaseConfig;
