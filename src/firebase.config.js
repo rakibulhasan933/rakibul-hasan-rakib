cac822e6b27d256e0338766c3f77916441ca6305
rakibulhasan933 <rakibulmd933@gmail.com>
2021-05-03 12:32:59 +0600

    final commit

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDHwxvjL-v1P9-cqRt560za5znXTBV-VJc",
+    authDomain: "my-protfulioe-final.firebaseapp.com",
+    projectId: "my-protfulioe-final",
+    storageBucket: "my-protfulioe-final.appspot.com",
+    messagingSenderId: "581989637289",
+    appId: "1:581989637289:web:615c8cada2a4ce87c79db8"
+};
+export default firebaseConfig;
