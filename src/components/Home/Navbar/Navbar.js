7b03f9b16ba267092cdbef35b16fedfe4a3bb6f6
rakibulhasan933 <rakibulmd933@gmail.com>
2021-05-03 15:30:14 +0600

    find

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../../Photos/logo.jpg';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="navbar fixed-top navbar-expand-lg navbar-light custom-navbar">
-            <a className="navbar-brand" href="/"><img width="50px" src={logo} alt="" /></a>
+            <Link className="navbar-brand" to="/"><img width="50px" src={logo} alt="" /></Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item p-3">
-                        <a className="text-decoration-none" href="/"><h5 className="custom-nav-item mr-5 ">Home</h5></a>
+                        <Link className="text-decoration-none" to="/"><h5 className="custom-nav-item mr-5 ">Home</h5></Link>
                     </li>
                     <li className="nav-item p-3">
-                        <a className="text-decoration-none" href="/about"><h5 className="custom-nav-item mr-5">About Me</h5></a>
+                        <Link className="text-decoration-none" to="/about"><h5 className="custom-nav-item mr-5">About Me</h5></Link>
                     </li>
                     <li className="nav-item p-3">
-                        <a className="text-decoration-none" href="/portfolio"><h5 className="custom-nav-item mr-5">Portfolio</h5></a>
+                        <Link className="text-decoration-none" to="/portfolio"><h5 className="custom-nav-item mr-5">Portfolio</h5></Link>
                     </li>
                     <li className="nav-item p-3">
-                        <a className="text-decoration-none" href="/blogs"><h5 className="custom-nav-item mr-5">Blog</h5></a>
+                        <Link className="text-decoration-none" to="/blogs"><h5 className="custom-nav-item mr-5">Blog</h5></Link>
                     </li>
                     <li className="nav-item p-3">
-                        <a className="text-decoration-none" href="/contact"><h5 className="custom-nav-item mr-5">Contact</h5></a>
+                        <Link className="text-decoration-none" to="/contact"><h5 className="custom-nav-item mr-5">Contact</h5></Link>
                     </li>
                 </ul>
             </div>
