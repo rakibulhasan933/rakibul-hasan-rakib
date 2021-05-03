18c47fcd8cec46c222385937ca998afe9d695047
rakibulhasan933 <rakibulmd933@gmail.com>
2021-05-03 12:47:04 +0600

    final host

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDHwxvjL-v1P9-cqRt560za5znXTBV-VJc",
-    authDomain: "my-protfulioe-final.firebaseapp.com",
-    projectId: "my-protfulioe-final",
-    storageBucket: "my-protfulioe-final.appspot.com",
-    messagingSenderId: "581989637289",
-    appId: "1:581989637289:web:615c8cada2a4ce87c79db8"
+    apiKey: "AIzaSyAzgy5FmNry8_-eH4kyN79_7XD9z-hcHwc",
+    authDomain: "rakibulhasanrakib-c4824.firebaseapp.com",
+    projectId: "rakibulhasanrakib-c4824",
+    storageBucket: "rakibulhasanrakib-c4824.appspot.com",
+    messagingSenderId: "160139478338",
+    appId: "1:160139478338:web:6861780aad926980d174db"
 };
 export default firebaseConfig;
